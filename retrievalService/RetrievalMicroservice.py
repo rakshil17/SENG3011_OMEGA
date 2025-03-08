3722d4d96e6f8ec60a5e4da6f5974aef1939376b
BobHell0 <anujan.cenan@gmail.com>
2025-03-08 20:54:11 +1100

    created flask app that will implement the microservice

---
+++ retrievalService/RetrievalMicroservice.py
@@ -0,0 +1,27 @@
+# ROUTES TO COMPLETE:
+# /retrieve
+# /list_files
+# /delete
+# /update
+
+from flask import Flask, request
+from BotoPullFile import BotoPullFile
+from secrets import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_S3_BUCKET_NAME, AWS_REGION
+
+app = Flask(__name__)
+
+@app.route('/v1/retrieve', methods=['GET'])
+def retrieve():
+    filename = request.form.get('filename')
+    puller = BotoPullFile()
+    return puller.pull(AWS_S3_BUCKET_NAME, filename)
+
+@app.route('/v1/delete', methods=['DELETE'])
+def delete():
+    filename = request.form.get('filename')
+    deleter =  
+
+
+
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', port=5001, debug = True) 
