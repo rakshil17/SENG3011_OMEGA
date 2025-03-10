a960372e3a1b53985260e97b0eedcca16f03b7a2
BobHell0 <anujan.cenan@gmail.com>
2025-03-10 12:38:19 +1100

    fixed up getting body params for my routes - also actually gave a return statement to the delete function

--- retrievalService/RetrievalMicroservice.py
+++ retrievalService/RetrievalMicroservice.py
@@ -6,30 +6,34 @@
 
 from flask import Flask, request
 from RetrievalInterface import RetrievalInterface
-from secrets import AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_S3_BUCKET_NAME, AWS_REGION
+import secrets
 import sys
 app = Flask(__name__)
 
+AWS_S3_BUCKET_NAME = "seng3011-omega-25t1-testing-bucket"
+# filename = "test_file.txt"
+
 @app.route('/v1/retrieve', methods=['GET'])
 def retrieve():
-    filename = request.form.get('filename')
-    puller = RetrievalInterface()
+    filename = request.get_json()['filename']
+    retrievalInterface = RetrievalInterface()
     try:
-        return puller.pull(AWS_S3_BUCKET_NAME, filename)
+        return retrievalInterface.pull(AWS_S3_BUCKET_NAME, filename)
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.retrieve) Exception: {e}")
+
         
 
 @app.route('/v1/delete', methods=['DELETE'])
 def delete():
-    filename = request.form.get('filename')
-    deleter =  RetrievalInterface()
+    filename = request.get_json()['filename']
+    retrievalInterface =  RetrievalInterface()
     try:
-        deleter.deleteOne(AWS_S3_BUCKET_NAME, filename)
+        retrievalInterface.deleteOne(AWS_S3_BUCKET_NAME, filename)
+        return f"Deleted {filename}"
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.delete) Exception: {e}")
 
-@app.route('/v1/delete', methods=['DELETE'])
 
 
 if __name__ == "__main__":
