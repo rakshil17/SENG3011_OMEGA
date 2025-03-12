c5885b6cc0cb948b8dcb47f146495a3a47a6a137
BobHell0 <anujan.cenan@gmail.com>
2025-03-12 19:26:05 +1100

    test files for different error cases

--- retrievalService/testing/test_get_file_from_dynamo.py
+++ retrievalService/testing/test_get_file_from_dynamo.py
@@ -22,6 +22,45 @@ class TestGetFileFromDynamo:
         assert retrievedFile.get('filename') == fileName
 
     @mock_aws
-    def test_get_
+    def test_get_file_from_wrong_user(self, test_table_two_users):
+        fileName = 'test-file.txt'
+        fileContent = 'some nice file content'
+        username = 'user1'
+        username2 = 'user2'
+        tableName = 'test-table'
+
+        retrievalInterface = RetrievalInterface()
+        retrievalInterface.pushToDynamo(fileName, fileContent, username, tableName)
+        
+        found, retrievedFile, index = retrievalInterface.getFileFromDynamo(fileName, username2, tableName)
+        assert not found
+
+    @mock_aws
+    def test_get_file_wrong_file_name(self, test_table):
+        fileName = 'test-file.txt'
+        fileContent = 'some nice file content'
+        username = 'user1'
+        tableName = 'test-table'
+
+        retrievalInterface = RetrievalInterface()
+        retrievalInterface.pushToDynamo(fileName, fileContent, username, tableName)
+        
+
+        found, retrievedFile, index = retrievalInterface.getFileFromDynamo('wrongFileName', username, tableName)
+        assert not found
+    
+    @mock_aws
+    def test_get_file_wrong_username(self, test_table):
+        fileName = 'test-file.txt'
+        fileContent = 'some nice file content'
+        username = 'user1'
+        tableName = 'test-table'
+
+        retrievalInterface = RetrievalInterface()
+        retrievalInterface.pushToDynamo(fileName, fileContent, username, tableName)
+        
+        with pytest.raises(Exception):
+            retrievalInterface.getFileFromDynamo(filename, 'wrongUsername', tableName)
+
 
 
