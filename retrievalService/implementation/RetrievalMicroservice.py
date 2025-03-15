9e803adfa4c71b1b5b16b8b4f677a68300c699b4
BobHell0 <anujan.cenan@gmail.com>
2025-03-15 15:55:25 +1100

    planning out routes

--- retrievalService/implementation/RetrievalMicroservice.py
+++ retrievalService/implementation/RetrievalMicroservice.py
@@ -12,10 +12,16 @@ app = Flask(__name__)
 AWS_S3_BUCKET_NAME = "seng3011-omega-25t1-testing-bucket"
 
 
-@app.route('/v1/retrieve/<filename>', methods=['GET'])
+@app.route('/v1/retrieve/<username>/<filename>/', methods=['GET'])
 def retrieve(filename: str):
     retrievalInterface = RetrievalInterface()
     try:
+        # try to retrieve the file immediately in the dynamoDB
+        # if there
+        # if not, get from S3, push to dynamo
+
+        # return content
+
         return retrievalInterface.pull(AWS_S3_BUCKET_NAME, filename)
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.retrieve) Exception: {e}")
@@ -24,12 +30,19 @@ def retrieve(filename: str):
 @app.route('/v1/delete', methods=['DELETE'])
 def delete():
     filename = request.get_json()['filename']
+    filename = request.get_json()['username']
+
     retrievalInterface = RetrievalInterface()
     try:
+        # delete from dynamodb
         retrievalInterface.deleteOne(AWS_S3_BUCKET_NAME, filename)
         return f"Deleted {filename}"
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.delete) Exception: {e}")
+
+@app.route('/v1/list/<username>', methods=['GET'])
+def getAll():
+    
 
 
 if __name__ == "__main__":
