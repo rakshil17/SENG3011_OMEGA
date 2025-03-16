9f4df980b11f47203652bfc637768fa4e461ef2a
BobHell0 <anujan.cenan@gmail.com>
2025-03-16 17:12:38 +1100

    use new fixture in conftest to simplify tests

--- retrievalService/testing/test_pull_from_s3.py
+++ retrievalService/testing/test_pull_from_s3.py
@@ -12,17 +12,10 @@ from ..implementation.RetrievalInterface import RetrievalInterface
 class TestPullFromS3:
     @mock_aws
     # successfully pull a file
-    def test_pull_file(self):
+    def test_pull_file(self, s3_mock):
         bucket_name = 'test-bucket'
         fileName = 'test-file.txt'
         file_content = 'Hello, Moto!'
-
-        # Create a mock S3 bucket and upload a test file.
-        s3 = boto3.client('s3')
-        s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
-            'LocationConstraint': 'ap-southeast-2'
-        })
-        s3.put_object(Bucket=bucket_name, Key=fileName, Body=file_content.encode('utf-8'))
 
         # Call the function to download and read the file.
         retrievalInterface = RetrievalInterface()
@@ -33,24 +26,17 @@ class TestPullFromS3:
 
     @mock_aws
     # try to pull a file that does not exist
-    def test_pull_nonexistent_file(self):
+    def test_pull_nonexistent_file(self, s3_mock):
         bucket_name = 'test-bucket'
-
-        s3 = boto3.client('s3')
-        s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={
-            'LocationConstraint': 'ap-southeast-2'})
-        s3.put_object(Bucket=bucket_name, Key='real-file', Body='file_content'.encode('utf-8'))
-
+        
         retrievalInterface = RetrievalInterface()
-        with pytest.raises(s3.exceptions.NoSuchKey):
+        with pytest.raises(s3_mock.exceptions.NoSuchKey):
             retrievalInterface.pull("test-bucket", "non-existent-file")
 
     @mock_aws
-    def test_pull_nonexistent_bucket(self):
-        s3 = boto3.client('s3')
-        s3.create_bucket(Bucket='real-bucket', CreateBucketConfiguration={
-            'LocationConstraint': 'ap-southeast-2'})
+    def test_pull_nonexistent_bucket(self, s3_mock):
         retrievalInterface = RetrievalInterface()
 
-        with pytest.raises(s3.exceptions.NoSuchBucket):
-            retrievalInterface.pull("test-bucket", "non-existent-file")
+        with pytest.raises(s3_mock.exceptions.NoSuchBucket):
+            retrievalInterface.pull('fake-bucket', "non-existent-file")
+
