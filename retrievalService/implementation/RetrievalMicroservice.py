4f28ae6434824c294f04fdcbc31dedcf73f597a6
BobHell0 <anujan.cenan@gmail.com>
2025-03-18 10:05:49 +1100

    changes to DynamoDB structure: content is now a list of date,closeVal pairs; there is also stockName

--- retrievalService/implementation/RetrievalMicroservice.py
+++ retrievalService/implementation/RetrievalMicroservice.py
@@ -7,7 +7,7 @@ app =  Flask(__name__)
 AWS_S3_BUCKET_NAME = "seng3011-omega-25t1-testing-bucket"
 DYNAMO_DB_NAME = "seng3011-test-dynamodb"
 
-@app.route('/v1/register', methods=['POST'])
+@app.route('/v1/register/', methods=['POST'])
 def register():
     username = request.get_json()['username']
     retrievalInterface = RetrievalInterface()
@@ -22,23 +22,31 @@ def register():
 @app.route('/v1/retrieve/<username>/<filename>/', methods=['GET'])
 def retrieve(username, filename: str):
     retrievalInterface = RetrievalInterface()
-    filename = f"{username}_{filename}"
+    filenameS3 = f"{username}_{filename}"     # need to think about Rakshil's file formatting here
     try:
         found, content, index = retrievalInterface.getFileFromDynamo(filename, username, DYNAMO_DB_NAME)
+
         if found:
-            return content
+            return {
+                'StockName': filename.removesuffix('.txt'),
+                'StockData': content
+            }
         else:
-            content = retrievalInterface.pull(AWS_S3_BUCKET_NAME, f"{filename}")
-            
+            content = retrievalInterface.pull(AWS_S3_BUCKET_NAME, f"{filenameS3}")
+            # need to format Rakshil's S3 content format into DynamoDB content
             retrievalInterface.pushToDynamo(filename, content, username, DYNAMO_DB_NAME)
-            return content
+            found, content, index = retrievalInterface.getFileFromDynamo(filename, username, DYNAMO_DB_NAME)
+            return {
+                'StockName': filename.removesuffix('.txt'),     # will change depending on what Rakshil did
+                'StockData': content
+            }
 
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.retrieve) Exception: {e}\n")
         return f"{e}"
 
 
-@app.route('/v1/delete', methods=['DELETE'])
+@app.route('/v1/delete/', methods=['DELETE'])
 def delete():
     filename = request.get_json()['filename']
     username = request.get_json()['username']
@@ -51,9 +59,14 @@ def delete():
     except Exception as e:
         sys.stderr.write(f"(RetrievalMicroservice.delete) Exception: {e}")
 
-# @app.route('/v1/list/<username>', methods=['GET'])
-# def getAll():
+@app.route('/v1/list/<username>/', methods=['GET'])
+def getAll(username):
+    retrievalInterface = RetrievalInterface()
+    try:
+        return retrievalInterface.listUserFiles(username, DYNAMO_DB_NAME)
 
+    except Exception as e:
+        sys.stderr.write(f"(RetrievalMicroservice.delete) Exception: {e}")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
