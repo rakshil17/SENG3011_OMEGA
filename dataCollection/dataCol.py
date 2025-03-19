e1078aa08704886c20f0fe72d233afc4aa83c6e0
rakshil17 <rakshilmali@gmail.com>
2025-03-19 15:48:23 +1100

    added the req.txt and dockerfile

--- dataCollection/dataCol.py
+++ dataCollection/dataCol.py
@@ -160,5 +160,5 @@ def check_stock():
             return jsonify({"error": f"Error checking S3: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
 
