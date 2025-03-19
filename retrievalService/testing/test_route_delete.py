6506bb21791e0033860fce46cd73ebb93b736088
BobHell0 <anujan.cenan@gmail.com>
2025-03-20 09:27:06 +1100

    file tests for delete route

---
+++ retrievalService/testing/test_route_delete.py
@@ -0,0 +1,33 @@
+import pytest
+from moto import mock_aws
+import json
+
+# from ..implementation.RetrievalMicroservice import app
+
+
+@pytest.mark.filterwarnings(r"ignore:datetime.datetime.utcnow\(\) is deprecated:DeprecationWarning")
+class TestDeleteRoute:
+    @mock_aws
+    def test_delete(self, rootdir, client, s3_mock, test_table):
+        stockName = 'apple'
+        username = 'user1'
+
+        client.get(f"/v1/retrieve/{username}/{stockName}/")
+        res = client.delete("/v1/delete/", json={"username": username, "filename": stockName})
+
+        assert res.status_code == 200
+
+        res = client.delete("/v1/delete/", json={"username": username, "filename": stockName})
+        assert res.status_code == 400
+        assert json.loads(res.data)['FileNotFound'] is not None
+
+    @mock_aws
+    def test_invalid_username(self, rootdir, client, s3_mock, test_table):
+        stockName = 'apple'
+        username = 'user1'
+
+        client.get(f"/v1/retrieve/{username}/{stockName}/")
+        res = client.delete("/v1/delete/", json={"username": 'wrong-user', "filename": stockName})
+
+        assert res.status_code == 401
+        assert json.loads(res.data)['UserNotFound'] is not None
