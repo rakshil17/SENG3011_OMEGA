1a5e148e90edec3ac540f6db13cfd6a96fe7c439
BobHell0 <anujan.cenan@gmail.com>
2025-03-20 10:02:39 +1100

    linting

--- retrievalService/testing/conftest.py
+++ retrievalService/testing/conftest.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../implementation")))
-from ..implementation.RetrievalMicroservice import app as flask_app
+from ..implementation.RetrievalMicroservice import app as flask_app  # noqa: E402
 
 
 @pytest.fixture
